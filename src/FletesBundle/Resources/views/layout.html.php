fb14471c984358f9025dd90a06199cd055029851
gaston dubra <gaston.dubra@gmail.com>
2015-07-06 19:55:06 -0300

    bower setup

--- src/FletesBundle/Resources/views/layout.html.php
+++ src/FletesBundle/Resources/views/layout.html.php
@@ -3,10 +3,26 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title><?php $view['slots']->output('title', 'Fletes') ?></title>
-        <?php foreach ($view['assetic']->stylesheets(array('bootstrap/css/*','bundles/fletes/css/cc/*')) as $url): ?>
+        <?php foreach ($view['assetic']->stylesheets(
+                array(
+                        'vendor/bootstrap/dist/css/*',
+                        '@FletesBundle/Resources/public/css/cc/*'
+                ),
+                array('cssrewrite')
+                ) as $url): ?>
             <link rel="stylesheet" href="<?php echo $view->escape($url) ?>" />
         <?php endforeach ?>  
-        <?php foreach ($view['assetic']->javascripts(array('bundles/fletes/js/thirdparty/angular.min.js','bundles/fletes/js/thirdparty/lodash.min.js','bundles/fletes/js/thirdparty/*','bootstrap/js/*','bundles/fletes/js/cc/fletes-angular.js','bundles/fletes/js/cc/fletes-fn.js','bundles/fletes/js/cc/fletes-ui.js'),array(),
+        <?php foreach ($view['assetic']->javascripts(
+                array(
+                        'vendor/jquery/dist/*.min.js',
+                        'vendor/bootstrap/dist/js/*.min.js',
+                        'vendor/angular/angular.min.js',
+                        'vendor/angular-bootstrap/*-tpls.min.js',
+                        '@FletesBundle/Resources/public/js/thirdparty/*.js',
+                        '@FletesBundle/Resources/public/js/cc/fletes-angular.js',
+                        '@FletesBundle/Resources/public/js/cc/fletes-fn.js',
+                        '@FletesBundle/Resources/public/js/cc/fletes-ui.js'
+                        ),array(),
                 array('output' => 'js/compiled/js.js')) as $url): ?>
                 <script src="<?php echo $view->escape($url) ?>"></script>
         <?php endforeach ?>
--- web/bundles/fletes/js/index.js
+++
@@ -1,6 +0,0 @@
-angular.module('fletes')
-.controller("IndexCtrl",['$scope','$http','uiGmapGoogleMapApi'
-    , function ($scope,$http, GoogleMapApi) {
-    
-    
-}]);
